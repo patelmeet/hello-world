4671c45a6dbda07964fa5fff4369ab7593178559
Meet Patel <patelmeet953@gmail.com>
2017-10-27 21:43:39 +0530

    Update 412. Fizz Buzz.java

--- 412. Fizz Buzz.java
+++ 412. Fizz Buzz.java
@@ -33,8 +33,8 @@ public class Solution {
     public List<String> fizzBuzz(int n) {
         List<String> result = new ArrayList<String>(n+1);
         for(int i=1 ; i<=n ; i++){
-            if(i%3 == 0 && i%5 == 0)
-                result.add("FizzBuzz");
+            if(i%3 == 0 && i%5 == 0){
+                result.add("FizzBuzz");}
             else if(i%3 == 0)
                 result.add("Fizz");
             else if(i%5 == 0)
